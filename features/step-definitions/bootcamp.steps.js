30886a1a4ce77e8b47f10bcdf16381f7a5ed6abc
Tetiana <shulga.tati@gmail.com>
2022-12-03 16:54:34 +0200

    fix commit

--- features/step-definitions/bootcamp.steps.js
+++ features/step-definitions/bootcamp.steps.js
@@ -3,16 +3,18 @@ const { Given, When, Then } = require("@wdio/cucumber-framework");
 const homeUrl = "https://www.newegg.com/";
 const searchField = '[type="search"]';
 const searchButton = 'button[class="ico ico-search"]';
-const beastDealsTab = "#trendingBanner_720202 > span";
 const shopLogo = '[class="header2021-logo"]';
 
 Given("I open the home page", async () => {
   await browser.url(homeUrl);
-  await browser.pause(3000);
 });
 
 Given("I close the promo banner if it appears", async () => {
-  await browser.refresh();
+  try {
+    await $('[title="Tech Bells Ringing"]').isDisplayed();
+  } catch (err) {
+    await browser.refresh(err);
+  }
 });
 
 When("I entry the {word} in the search bar top middle", async (request) => {
@@ -25,12 +27,12 @@ When(`I click the search`, async () => {
 });
 
 Then("Check that at least one item appears", async () => {
-  const windowsItem = await $('[id="item_cell_9SIA25VGYM6011_1_0"]');
-  await expect(windowsItem).toExist();
+  const listItems = await $$("div.item-info");
+  await expect(listItems).toBeElementsArrayOfSize({ gte: 1 });
 });
 
 When("I open {string} tab", async (tab) => {
-  await $(beastDealsTab).click();
+  await $(`#trendingBanner_720202=${tab}`).click();
 });
 
 When("I click on Shop logo top right", async () => {
